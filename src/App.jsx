83f210a79c7c51c0f2105785b2747e1ed638f963
sagar_pant <sagarpant2k2@gmail.com>
2023-08-29 21:26:50 +0530

    dekhlio

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,33 @@ import {SnakeGameContextProvider,SnakeGameContext} from './context/GameContext';
 import Nav from "./components/Nav/Nav";
 import Register from "./components/Register/Register"; 
 import RankList from "./components/ranking/Ranking";
-import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter,Routes,Route, redirect} from 'react-router-dom';
 
 
 const App = ()=>{
+  const {gameState} = useContext(SnakeGameContext);
   return (
-    <SnakeGameContextProvider>
+    
       <BrowserRouter>
       <Nav/>
         <Routes>
           <Route path="/" element={<Register/>}></Route>
-          <Route path="/game" element={<Main></Main>}></Route>
+
+          <Route path="/game" element={<Main/>}></Route>
+          
           <Route path="/ranklist" element={<RankList/>}></Route>
         </Routes>
      
       </BrowserRouter>
-    </SnakeGameContextProvider>
+   
   );
 }
 
-export default App;
+export default ()=>{
+  return (
+    <SnakeGameContextProvider>
+    <App></App>
+  </SnakeGameContextProvider>
+  );
+  
+};
